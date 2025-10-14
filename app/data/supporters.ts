66283b21be7f37f9c013ed8edb45d5c08818d008
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-10-14 00:04:29 -0700

    add new supporters

--- app/data/supporters.ts
+++ app/data/supporters.ts
@@ -3,6 +3,7 @@ export const supporters = [
     name: "ElevenLabs",
     logo: "https://11labs-nonprd-15f22c1d.s3.eu-west-3.amazonaws.com/0b9cd3e1-9fad-4a5b-b3a0-c96b0a1f1d2b/elevenlabs-logo-black.svg",
   },
+
   {
     name: "Balsamiq",
     logo: "https://balsamiq.com/assets/logo-white.svg",
@@ -18,5 +19,13 @@ export const supporters = [
   {
     name: "DASG",
     logo: "https://www.deanza.edu/dasg/images/dasg-logo/dasg-logo-abreviation.png",
+  },
+  {
+    name: "kluster.ai",
+    logo: "https://www.kluster.ai/brand/logos/logo-primary-white.svg",
+  },
+  {
+    name: "StickerGiant",
+    logo: "https://www.stickergiant.com/media/athlete2/default/StickerGiant_Logo.png",
   },
 ];
