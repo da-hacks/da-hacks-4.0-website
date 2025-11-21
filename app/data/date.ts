afc75d1a565212494d152aefa717a1a42cd01992
Ben <10395903+Bongs237@users.noreply.github.com>
2025-11-21 10:11:54 -0800

    Change start time to 10

--- app/data/date.ts
+++ app/data/date.ts
@@ -1,3 +1,3 @@
-// Hackathon starts at 11 AM on November 21st, 2025 and ends at 4 PM on November 22nd, 2025
-export const hackathonStartTime = "2025-11-21T11:00:00";
+// Hackathon starts at 10AM on November 21st, 2025 and ends at 4 PM on November 22nd, 2025
+export const hackathonStartTime = "2025-11-21T10:00:00";
 export const hackathonEndTime = "2025-11-22T16:00:00";
